96b7b94c97f086dcd1bbae644a69615c0108726b
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-08 22:31:45 +0200

    python operator test

--- dags/newdirectory.py
+++ dags/newdirectory.py
@@ -23,32 +23,19 @@ default_args = {
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
+
 def create_dir():
    os.makedirs("test_dir")
-   return print("created") 
-
+   print("created") 
 
 dag = DAG("new_directory", default_args=default_args, schedule_interval=timedelta(1))
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = PythonOperator(task_id="create_dir", python_callable=create_dir, dag=dag)
+t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 
 t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
 
-templated_command = """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-"""
-
-t3 = BashOperator(
-    task_id="templated",
-    bash_command=templated_command,
-    params={"my_param": "Parameter I passed in"},
-    dag=dag,
-)
+t3 = PythonOperator(task_id="create_dir", python_callable=create_dir, dag=dag)
 
 t2.set_upstream(t1)
 t3.set_upstream(t1)
