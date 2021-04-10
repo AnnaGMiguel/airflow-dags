70ba68006facbde46e9939d6ada497a319472907
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-10 21:57:50 +0200

    add new folder

--- dags/newdirectory.py
+++ dags/newdirectory.py
@@ -5,7 +5,7 @@ Code to create a new directory
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
-import os
+
 
 
 default_args = {
@@ -23,7 +23,7 @@ default_args = {
 dag = DAG("new_directory", default_args=default_args, schedule_interval=timedelta(1))
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(task_id="new_dir", bash_command="mkdir ~/dags/test_dir", dag=dag)
+t1 = BashOperator(task_id="new_dir", bash_command="mkdir ~/test_dir", dag=dag)
 
 t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
 
