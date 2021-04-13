86cc0212d91230e383ccd1e630d1f24f713f4ae3
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-13 21:37:42 +0200

    Update store_dag.py

--- dags/store_dag.py
+++ dags/store_dag.py
@@ -1,6 +1,9 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+from datacleaner import data_cleaner
 
 default_args = {
     'owner' : 'Airflow',
@@ -9,7 +12,9 @@ default_args = {
     'retry_delay' : timedelta(seconds=5)
 }
 
-dag = DAG('store_dag', default_args = default_args, schedule_interval = '@daily', catchup = False)
+dag = DAG('store_dag', default_args=default_args, schedule_interval='@daily', catchup=False)
 
-t1 = BashOperator(task_id = 'check_file_exists', bash_command = 'shasum ~ /store_files/raw_store_transactions.csv', retries = 2,
- retry_delay = timedelta(seconds=15), dag = dag)
+t1 = BashOperator(task_id='check_file_exists', bash_command='shasum ~ /store_files/raw_store_transactions.csv', retries=2,
+ retry_delay=timedelta(seconds=15), dag=dag)
+
+t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner, dag=dag)
