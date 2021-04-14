d9f73e4d1a386990e72114c9a6fc1dfab9b16e91
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-14 20:56:15 +0200

    Update owner

--- dags/store_dag.py
+++ dags/store_dag.py
@@ -6,13 +6,13 @@ from airflow.operators.python_operator import PythonOperator
 from datacleaner import data_cleaner
 
 default_args = {
-    'owner' : 'Airflow',
+    'owner' : 'airflow',
     'start_date' : datetime(2021, 4, 13),
     'retries' : 1,
     'retry_delay' : timedelta(seconds=5)
 }
 
-dag = DAG('store_dag', default_args=default_args, schedule_interval='@daily', catchup=False)
+dag = DAG('store_dag', default_args=default_args, schedule_interval=timedelta(1))# schedule_interval='@daily', catchup=False)
 
 t1 = BashOperator(task_id='check_file_exists', bash_command='shasum $AIRFLOW__CORE__DAGS_FOLDER/raw_store_transactions.csv', retries=2,
  retry_delay=timedelta(seconds=15), dag=dag)
