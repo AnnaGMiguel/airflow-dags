11ce8bb08076df4ed7d56ba41e165c759dbc9ecb
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-18 22:02:28 +0200

    new test_dag

---
+++ dags/test_dag.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+from datetime import datetime, timedelta
+
+def my_func():
+    print('test connection')
+    
+ 
+with DAG('test_dag', description='Python DAG', schedule_interval=timedelta(1), start_date=datetime(2021, 4, 17), catchup=False) as dag:
+	dummy_task 	= DummyOperator(task_id='dummy_task', retries=3)
+	python_task	= PythonOperator(task_id='python_task', python_callable=my_func)
+ 
+	dummy_task >> python_task
