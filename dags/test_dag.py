a2a7846fa63942d29c18bf57e86bb7b860013ef1
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-20 21:40:13 +0200

    new __init__.py

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -4,18 +4,16 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 
 from datetime import datetime, timedelta
-#from datacleaner import data_cleaner
+from datacleaner import data_cleaner
 
 def my_func():
     print('test connection')
     
  
 with DAG('tests_dag', description='Python DAG', schedule_interval=timedelta(1), start_date=datetime(2021, 4, 17), catchup=False) as dag:
-	dummy_task 	= DummyOperator(task_id='dummy_task', retries=3)
+    dummy_task 	= DummyOperator(task_id='dummy_task', retries=3)
 	python_task	= PythonOperator(task_id='python_task', python_callable=my_func)
 	t1=BashOperator(task_id='check_file_exists', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
-	#t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
+	t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
 	#t3=BashOperator(task_id='check_new_file', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/clean_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
-	dummy_task >> python_task >> t1
-	# >> t2
- 
+	dummy_task >> python_task >> t1 # >> t2
