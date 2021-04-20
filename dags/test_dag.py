2f852ce34f3ea4f6486c6fadf2c96cbbd120294f
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-20 20:55:41 +0200

    updated test_dag

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -14,7 +14,7 @@ with DAG('test_dag', description='Python DAG', schedule_interval=timedelta(1), s
 	dummy_task 	= DummyOperator(task_id='dummy_task', retries=3)
 	#python_task	= PythonOperator(task_id='python_task', python_callable=my_func)
 	t1=BashOperator(task_id='check_file_exists', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
-	t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
+	#t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
 	#t3=BashOperator(task_id='check_new_file', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/clean_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
-	dummy_task >> t1 >> t2
+	dummy_task >> t1# >> t2
  
