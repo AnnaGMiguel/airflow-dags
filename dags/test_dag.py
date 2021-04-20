d7b250af0018dc065cf92b3c43be556e391df408
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-20 22:36:30 +0200

    test2

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -33,7 +33,7 @@ def data_cleaner():
 	for to_clean in ['MRP', 'CP', 'DISCOUNT', 'SP']:
         	df[to_clean] = df[to_clean].map(lambda x: remove_dollar(x))
 	
-	df.to_csv(airflow_home+'/dags/repo/store_files/clean_store_transactions.csv', index=False)
+	df.to_csv(airflow_home+'/tmp/clean_store_transactions.csv', index=False)
 
  
 with DAG('tests_dag', description='Python DAG', schedule_interval=timedelta(1), start_date=datetime(2021, 4, 17), catchup=False) as dag:
@@ -41,5 +41,5 @@ with DAG('tests_dag', description='Python DAG', schedule_interval=timedelta(1), 
 	python_task	= PythonOperator(task_id='python_task', python_callable=my_func)
 	t1=BashOperator(task_id='check_file_exists', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
 	t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
-	#t3=BashOperator(task_id='check_new_file', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/clean_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
-	dummy_task >> python_task >> t1 >> t2
+	t3=BashOperator(task_id='check_new_file', bash_command='cat $AIRFLOW_HOME/tmp/clean_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
+	dummy_task >> python_task >> t1 >> t2 >> t3
