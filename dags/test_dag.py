90265945502f39c7c7c542dcf949a77c74d3b725
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-20 21:51:16 +0200

    updated data_cleaner

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -4,11 +4,38 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 
 from datetime import datetime, timedelta
-from datacleaner import data_cleaner
+#from datacleaner import data_cleaner
 
 def my_func():
     print('test connection')
     
+def data_cleaner():
+
+    import pandas as pd
+    import re
+
+    df = pd.read_csv("$AIRFLOW_HOME/dags/repo/store_files/raw_store_transactions.csv")
+
+    def clean_store_location(st_loc):
+        return re.sub(r'[^\w\s]', '', st_loc).strip()
+
+    def clean_product_id(pd_id):
+        matches = re.findall(r'\d+', pd_id)
+        if matches:
+            return matches[0]
+        return pd_id
+
+    def remove_dollar(amount):
+        return float(amount.replace('$', ''))
+
+    df['STORE_LOCATION'] = df['STORE_LOCATION'].map(lambda x: clean_store_location(x))
+    df['PRODUCT_ID'] = df['PRODUCT_ID'].map(lambda x: clean_product_id(x))
+
+    for to_clean in ['MRP', 'CP', 'DISCOUNT', 'SP']:
+        df[to_clean] = df[to_clean].map(lambda x: remove_dollar(x))
+
+    df.to_csv('$AIRFLOW_HOME/dags/repo/store_files/clean_store_transactions.csv', index=False)
+
  
 with DAG('tests_dag', description='Python DAG', schedule_interval=timedelta(1), start_date=datetime(2021, 4, 17), catchup=False) as dag:
 	dummy_task 	= DummyOperator(task_id='dummy_task', retries=3)
