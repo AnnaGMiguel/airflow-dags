cc9457741062e69a8066a6d3e4273dc755537001
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-20 20:59:45 +0200

    tested task_id

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -10,7 +10,7 @@ def my_func():
     print('test connection')
     
  
-with DAG('test_dag', description='Python DAG', schedule_interval=timedelta(1), start_date=datetime(2021, 4, 17), catchup=False) as dag:
+with DAG('tests_dag', description='Python DAG', schedule_interval=timedelta(1), start_date=datetime(2021, 4, 17), catchup=False) as dag:
 	dummy_task 	= DummyOperator(task_id='dummy_task', retries=3)
 	#python_task	= PythonOperator(task_id='python_task', python_callable=my_func)
 	t1=BashOperator(task_id='check_file_exists', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
