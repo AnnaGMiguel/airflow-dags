c3ba4cf768fe3b9dba457cb08ef7148727c25cd1
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-24 20:57:56 +0200

    first PostgresOperator

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -2,6 +2,8 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.postgres_operator import PostgresOperator
+
 import pandas as pd
 import re
 import os
@@ -36,10 +38,11 @@ def data_cleaner():
 	df.to_csv('/tmp/clean_store_transactions.csv', index=False)
 
  
-with DAG('tests_dag', description='Python DAG', schedule_interval=timedelta(1), start_date=datetime(2021, 4, 17), catchup=False) as dag:
+with DAG('tests_dag', description='Python DAG', schedule_interval=timedelta(1), start_date=datetime(2021, 4, 17), template_searchpath=(os.environ["AIRFLOW_HOME"]+'/dag/repo/postgres_files'), catchup=False) as dag:
 	dummy_task 	= DummyOperator(task_id='dummy_task', retries=3)
 	python_task	= PythonOperator(task_id='python_task', python_callable=my_func)
 	t1=BashOperator(task_id='check_file_exists', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
 	t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
-	t3=BashOperator(task_id='check_new_file', bash_command='cat /tmp/clean_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
-	dummy_task >> python_task >> t1 >> t2 >> t3
+	t3=BashOperator(task_id='check_new_file', bash_command='$AIRFLOW_HOME/dags/repo/store_files/clean_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
+	t4 = PostgresOperator(task_id='create_postgres_table',postgres_conn_id="postgres_conn", sql="create_table.sql")
+	dummy_task >> python_task >> t1 >> t2 >> t3 >> t4
