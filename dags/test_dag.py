ab73df240a444bbb63e3848ede355cbdff804cc6
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-28 20:56:36 +0200

    postgres connection

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -44,5 +44,5 @@ with DAG('tests_dag', description='Python DAG', schedule_interval=timedelta(1), 
 	t1=BashOperator(task_id='check_file_exists', bash_command='cat $AIRFLOW_HOME/dags/repo/store_files/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
 	t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
 	#t3=BashOperator(task_id='check_new_file', bash_command='cat /tmp/store_files/clean_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
-	t3 = PostgresOperator(task_id='create_postgres_table',postgres_conn_id="postgres_conn", sql="create_table.sql")
+	t3 = PostgresOperator(task_id='create_postgres_table',postgres_conn_id="postgres_default", sql="create_table.sql")
 	dummy_task >> python_task >> t1 >> t2 >> t3
