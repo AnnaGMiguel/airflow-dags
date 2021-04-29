97a25ec8c0fad691cfb7f3f890eda156ae9082f8
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-04-29 22:30:09 +0200

    test5

--- postgres_files/insert_into_table.sql
+++ postgres_files/insert_into_table.sql
@@ -1,1 +1,1 @@
-COPY clean_store_transactions(STORE_ID,STORE_LOCATION,PRODUCT_CATEGORY,PRODUCT_ID,MRP,CP,DISCOUNT,SP,Date) FROM '/tmp/clean_store_transactions.csv' DELIMITER ',' CSV HEADER;
+\copy clean_store_transactions(STORE_ID,STORE_LOCATION,PRODUCT_CATEGORY,PRODUCT_ID,MRP,CP,DISCOUNT,SP,Date) FROM '/tmp/clean_store_transactions.csv' DELIMITER ',' CSV HEADER;
