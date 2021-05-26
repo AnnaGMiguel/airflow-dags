2e7aeb4db98310f68b5b1cdbe2a9028d8fc5e8aa
AnnaGMiguel <anna_goncalves@gec.inatel.br>
2021-05-26 20:56:22 +0200

    pool_demo

---
+++ dags/pool_demo.py
@@ -0,0 +1,15 @@
+import airflow
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2021,26,5),
+}
+
+with DAG("pools", default_args=default_args,schedule_interval=timedelta(1)) as dag:
+    t1 = BashOperator(task_id="task-1", bash_command="sleep 5",pool ="pool_1")
+    t2 = BashOperator(task_id="task-2", bash_command="sleep 5",pool ="pool_1")
+    t3 = BashOperator(task_id="task-3", bash_command="sleep 5",pool ="pool_2")
+    t4 = BashOperator(task_id="task-4", bash_command="sleep 5",pool ="pool_2")
